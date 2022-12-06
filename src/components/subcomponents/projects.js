122cf7e2bd324b1a456f65dfa8f2781238c67f9a
Deepargh Chatterjee <chatterjeedeep2018@gmail.com>
2022-12-07 04:03:31 +0530

    added projects

--- src/components/subcomponents/projects.js
+++ src/components/subcomponents/projects.js
@@ -14,6 +14,8 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function projects() {
     const [open1, setOpen1] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+    const [open3, setOpen3] = React.useState(false);
 
   return (
     <Card sx={{ minWidth: 275, margin:10 , marginTop:5, marginBottom:5}}>
@@ -30,9 +32,9 @@ function projects() {
                         {/*Enter project image here*/}
                         <CardMedia component="img" image={process.env.PUBLIC_URL + '/project1.png'} alt="project1"/>
                         <CardContent>
-                            <div style={{display:"flex", justifyContent:"center"}}>
+                            <div style={{display:"inline"}}>
                                 {/*Enter project name here*/}
-                                <Typography variant="h4">Questly&nbsp;</Typography>
+                                <Typography variant="h4">Questly&nbsp;</Typography><br/>
                                 <Fab variant="extended" color="primary" aria-label="add" onClick={()=>setOpen1(true)}>
                                     See project &nbsp;
                                     <ArrowForwardIosIcon />
@@ -97,6 +99,161 @@ function projects() {
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={()=>setOpen1(false)}>Close</Button>
+                    </DialogActions>
+                </Dialog>
+                {/*================================================================================================
+                =================================================================================================== */}
+                {/* Project Item */}
+                <Grid item xs={4} sx={{display:{lg:'inline', md:'none', sm:'none', xs:'none'}}}>
+                    <Card variant="outlined" sx={{borderRadius:10,justifyContent:"center"}}>
+                        {/*Enter project image here*/}
+                        <CardMedia component="img" image={process.env.PUBLIC_URL + '/project2.png'} alt="project1"/>
+                        <CardContent>
+                            <div style={{display:"inline"}}>
+                                {/*Enter project name here*/}
+                                <Typography variant="h4">Sonic Revolution&nbsp;</Typography><br/>
+                                <Fab variant="extended" color="primary" aria-label="add" onClick={()=>setOpen2(true)}>
+                                    See project &nbsp;
+                                    <ArrowForwardIosIcon />
+                                </Fab>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                {/*Responsive Project Item */}
+                <Grid item md={6} sm={12} sx={{display:{lg:'none', md:'inline'}}}>
+                    <Card variant="outlined" sx={{borderRadius:10,justifyContent:"center"}}>
+                        {/*Enter project image here*/}
+                        <CardMedia component="img" image={process.env.PUBLIC_URL + '/project2.png'} alt="project1"/>
+                        <CardContent>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                {/*Enter project name here*/}
+                                <Typography variant="h4">Sonic Revolution&nbsp;</Typography>
+                                <Fab variant="extended" color="primary" aria-label="add" onClick={()=>setOpen2(true)}>
+                                    <ArrowForwardIosIcon />
+                                </Fab>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                {/* Project Description */}
+                <Dialog
+                    open={open2}
+                    onClose={()=>setOpen2(false)}
+                    scroll='paper'
+                    aria-labelledby="scroll-dialog-title"
+                    aria-describedby="scroll-dialog-description"
+                >
+                    {/*Enter project name here*/}
+                    <DialogTitle id="scroll-dialog-title">Questly</DialogTitle>
+                    <DialogContent dividers={true}>
+                        {/*Enter project image here*/}
+                        <img src={process.env.PUBLIC_URL + '/project2.png'} alt="project1" style={{width:"50%", height:"50%", margin:"25%", marginTop:"5%", marginBottom:"0%"}}/>
+                        <DialogContentText
+                            id="scroll-dialog-description"
+                            tabIndex={-1}
+                        >
+                            {/*Enter project description here*/}
+                            Sonic Revolution is an audio/sound generating app that aims to help beginner game
+                            developers create their own copyright-free original in-game music and sound effects. It
+                            also has a web forum where users can upload their own sound effects.{<><br/><br/></>}
+                            The audio generation is done in a procedural manner where we have a concept of
+                            “tracks” and “tones”. A “tone” can be created by choosing a musical note, octave, and
+                            duration which is then added to a track. We can add multiple tones on a track and overlay
+                            multiple tracks to generate any kind of audio effect.{<><br/><br/></>}
+                            The companion website, Sonic Revolution Forum, allows users to share their own
+                            sound effects with other game developers where anyone can listen and download audio
+                            files uploaded by others.
+                        </DialogContentText>
+                        <div style={{display:"flex",justifyContent:"space-evenly", margin:"2%"}}>
+                            {/*Enter project links here*/}
+                            <Button variant="outlined" color="primary" href="https://github.com/supanthapaul/sonic-revolution" target="_blank" rel="noopener noreferrer" >Github Repo</Button>
+                            <Button variant="outlined" color="primary" href={process.env.PUBLIC_URL + '/files/Sonic_Revolution_Report_Final.pdf'} download="Sonic_Revolution_Report_Final.pdf" rel="noopener noreferrer" >Project Report</Button>
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={()=>setOpen2(false)}>Close</Button>
+                    </DialogActions>
+                </Dialog>
+                {/*================================================================================================
+                =================================================================================================== */}
+                {/* Project Item */}
+                <Grid item xs={4} sx={{display:{lg:'inline', md:'none', sm:'none', xs:'none'}}}>
+                    <Card variant="outlined" sx={{borderRadius:10,justifyContent:"center"}}>
+                        {/*Enter project image here*/}
+                        <CardMedia component="img" image={process.env.PUBLIC_URL + '/project3.png'} alt="project3"/>
+                        <CardContent>
+                            <div style={{display:"inline"}}>
+                                {/*Enter project name here*/}
+                                <Typography variant="h4">MindPeace&nbsp;</Typography><br/>
+                                <Fab variant="extended" color="primary" aria-label="add" onClick={()=>setOpen3(true)}>
+                                    See project &nbsp;
+                                    <ArrowForwardIosIcon />
+                                </Fab>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                {/*Responsive Project Item */}
+                <Grid item md={6} sm={12} sx={{display:{lg:'none', md:'inline'}}}>
+                    <Card variant="outlined" sx={{borderRadius:10,justifyContent:"center"}}>
+                        {/*Enter project image here*/}
+                        <CardMedia component="img" image={process.env.PUBLIC_URL + '/project3.png'} alt="project3"/>
+                        <CardContent>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                {/*Enter project name here*/}
+                                <Typography variant="h4">MindPeace&nbsp;</Typography>
+                                <Fab variant="extended" color="primary" aria-label="add" onClick={()=>setOpen3(true)}>
+                                    <ArrowForwardIosIcon />
+                                </Fab>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                {/* Project Description */}
+                <Dialog
+                    open={open3}
+                    onClose={()=>setOpen3(false)}
+                    scroll='paper'
+                    aria-labelledby="scroll-dialog-title"
+                    aria-describedby="scroll-dialog-description"
+                >
+                    {/*Enter project name here*/}
+                    <DialogTitle id="scroll-dialog-title">Questly</DialogTitle>
+                    <DialogContent dividers={true}>
+                        {/*Enter project image here*/}
+                        <img src={process.env.PUBLIC_URL + '/project3.png'} alt="project3" style={{width:"50%", height:"50%", margin:"25%", marginTop:"5%", marginBottom:"0%"}}/>
+                        <DialogContentText
+                            id="scroll-dialog-description"
+                            tabIndex={-1}
+                        >
+                            {/*Enter project description here*/}
+                            MindPeace is an online platform that is focused on mental health, its awareness, and building 
+                            a community that supports one another in times of need. MindPeace not only has dedicated spaces 
+                            for each age group but also has branched out to address many mental health issues other than
+                            depression-like burnout, anxiety, PTSD, and eating disorders. One of the objectives of the 
+                            project is to create a safe space and easy access to genuine resources.
+                            {<><br/><br/></>}
+                            The objectives of this project are multi-faceted. One of the major objectives of MindPeace
+                            is to provide good and effective mental health care, accessible for all regardless of age and status,
+                            and to eliminate any social stigma by normalizing it. Also, a big motivation was to integrate some
+                            of the most useful features from different applications into one space.
+                            {<><br/><br/></>}
+                            We aim to connect some of the best professionals working in the field to every person in need and
+                            get them timely help. Also, an assessment plan through which an individual can evaluate their
+                            condition and get help accordingly.
+                            {<><br/><br/></>}
+                            Here, with the consideration of every concerned person, we have provided reliable and tested
+                            resources and ways through which one can treat themselves or someone close to them.
+                        </DialogContentText>
+                        <div style={{display:"flex",justifyContent:"space-evenly", margin:"2%"}}>
+                            {/*Enter project links here*/}
+                            <Button variant="outlined" color="primary" href="https://github.com/supanthapaul/mindpeace" target="_blank" rel="noopener noreferrer" >Github Repo</Button>
+                            <Button variant="outlined" color="primary" href={process.env.PUBLIC_URL + '/files/MindPeace Report_Final.pdf'} download="MindPeace Report_Final.pdf" rel="noopener noreferrer" >Project Report</Button>
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={()=>setOpen3(false)}>Close</Button>
                     </DialogActions>
                 </Dialog>
                 {/*================================================================================================
