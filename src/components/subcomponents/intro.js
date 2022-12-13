28a234518a5ad59e664350c8912fff30bfce0be7
Deepargh Chatterjee <chatterjeedeep2018@gmail.com>
2022-12-14 03:45:12 +0530

    updated intro

--- src/components/subcomponents/intro.js
+++ src/components/subcomponents/intro.js
@@ -3,19 +3,33 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import {FaLinkedin, FaInstagram, FaTwitter} from "react-icons/fa";
+import { Paper } from "@mui/material";
 
 function intro(){
     return(
         <Card sx={{ minWidth: 275, margin:5}}>
           <CardContent>
+            {/* Profile Picture */}
+            <Paper elevation={0} style={{width:"100%", height:"5%"}} sx={{display:{md:'inline', sm:'none', xs:'none'}}}>
+              <img src={process.env.PUBLIC_URL + '/profileArt.jpg'} alt="profile art" style={{width:"100%", height:"5%"}}/>
+              <Paper elevation={3} style={{width:"20%", height:"20%", borderWidth:"50%", borderStyle:"solid"}} sx={{marginTop:"-15%"}}>
+                <img src={process.env.PUBLIC_URL + '/profile.png'} alt="profile" style={{width:"100%", height:"100%"}}/>
+              </Paper>
+            </Paper>
+            <Paper elevation={0} style={{width:"100%", height:"5%"}} sx={{display:{md:'none', sm:'inline'}}}>
+              <img src={process.env.PUBLIC_URL + '/profile.png'} alt="profile" style={{width:"70%", height:"70%",marginLeft:"15%"}}/>
+            </Paper>
+            <br/><br/>
             {/* Name */}
-            <img src={process.env.PUBLIC_URL + '/profileArt.jpg'} alt="profile art" style={{width:"100%", height:"5%"}}/>
-            <img src={process.env.PUBLIC_URL + '/profile.png'} alt="profile" style={{width:"10%", height:"10%",}}/>
-            <Typography variant="h2" >
+            <Typography variant="h2" sx={{display:{md:'inline', sm:'none', xs:'none'}}}>
                 Hello! I'm Deepargh Chatterjee.
                 <span role="img" aria-label="waving-hand">👋</span> 
             </Typography>
-            <br/>
+            <Typography variant="h3" sx={{display:{md:'none',sm:'inline'}}}>
+                Hello! I'm Deepargh Chatterjee.
+                <span role="img" aria-label="waving-hand">👋</span> 
+            </Typography>
+            <br/><br/>
             {/* About */}
             <Typography variant="h5">
                 I'm a Full-stack developer and a UI/UX designer. And I'm specially good at Frontend Development.
@@ -29,7 +43,7 @@ function intro(){
                 I'm a very friendly person and I love to make new friends. I'm always ready to connect with new people. Here are my social media handles:
                 <br/><br/>
                 {/* Social Media Handles */}
-                <a href="https://www.linkedin.com/in/deepargh-chatterjee-0b1b1b1b1/" target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#ffcb74"}}>
+                <a href="https://www.linkedin.com/in/deepargh-chatterjee-086662198/" target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#ffcb74"}}>
                   <FaLinkedin style={{fontSize:"1.5rem"}} size={30}/> &nbsp;&nbsp;
                 </a>
                 <a href="https://www.instagram.com/chatterjeedeep08/" target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#ffcb74"}}>
